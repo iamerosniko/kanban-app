1639c73427e1223621bb2582535fb525f6f33e7b
Eros Niko Alvarez <iamerosniko@yahoo.com>
2016-11-28 16:06:39 -0500

    dnd 2

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import {compose} from 'redux';
+import {DragSource, DropTarget} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
 
-export default ({children, ...props}) => (
-    <div {...props}>
-        {children}
-    </div>
-);
+const Note = ({
+    connectDragSource, connectDropTarget,
+    children, ...props
+}) => {
+    return compose(connectDragSource, connectDropTarget)(
+        <div {...props}>
+            {children}
+        </div>
+    );  
+};
+const noteSource = {
+    beginDrag(props) {
+        console.log('begin dragging note', props);
+        return {};
+    }
+};
+export default DragSource(ItemTypes.NOTE, noteSource, connect => ({
+    connectDragSource: connect.dragSource()
+}))(Note)
