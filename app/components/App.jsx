c5588b4c6b24c29b99500882fd3c403f08b5b86e
Eros Niko Alvarez <iamerosniko@yahoo.com>
2016-11-28 15:08:15 -0500

    dynamic store

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,27 +4,12 @@ import uuid from 'uuid';
 import connect from '../libs/connect';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            notes: [
-                {
-                    id: uuid.v4(),
-                    task: 'Learn React'
-                },
-                {
-                    id: uuid.v4(),
-                    task: 'Do laundry'
-                }
-            ]
-        };
-    }
+    
     
     render() {
-        const {notes} = this.state;
+        const {notes} = this.props;
         return (
             <div>
-                {this.props.test}
                 <button className="add-note" onClick={this.addNote}>+</button>
                 <Notes 
                     notes={notes} 
@@ -75,6 +60,6 @@ class App extends React.Component {
     }
 }
 
-export default connect(() => ({
-    test: 'test'
+export default connect(({notes}) => ({
+    notes
 }))(App)
