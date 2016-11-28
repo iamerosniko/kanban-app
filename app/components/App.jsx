15d92f1377b90800f746db52098fc7fa31a2487f
Eros Niko Alvarez <iamerosniko@yahoo.com>
2016-11-28 12:50:08 -0500

    up

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import Notes from './Notes';
+import uuid from 'uuid';
 
+const notes = [
+    {
+        id: uuid.v4(),
+        task: 'Learn React'
+    },
+    {
+        id: uuid.v4(),
+        task: 'Do laundry'
+    }
+];
 export default () => (
     <div>
-        <button onClick={() => console.log('add note')}>+</button>
-        <Notes />
+        <button onClick={
+            () => {
+                console.log('add note')
+            }
+        }>+</button>
+        <Notes notes={notes} />
     </div>
 );
