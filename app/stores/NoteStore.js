b939762aeb4bc9320b57cd985c43a9a198d3b09e
Eros Niko Alvarez <iamerosniko@yahoo.com>
2016-11-28 15:17:04 -0500

    Store Action

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -17,12 +17,23 @@ export default class NoteStore {
         ];
     }
     create(note) {
-        console.log('create note', note);
+        this.setState({
+            notes: this.notes.concat(note)
+        });
     }
     update(updatedNote) {
-        console.log('update note', updatedNote);
+        this.setState({
+            notes: this.notes.map(note => {
+                if(note.id === updatedNote.id) {
+                    return Object.assign({}, note, updatedNote);
+                }
+                return note;
+            })
+        });
     }
     delete(id) {
-        console.log('delete note', id);
+        this.setState({
+            notes: this.notes.filter(note => note.id !== id)
+        });
     }
 }
